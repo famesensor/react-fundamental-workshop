1e05f356220cc8c4f8c22bb04dbf18f9ece08dc2
famesensor <famfew25@gmail.com>
2020-03-17 01:58:38 +0700

    :zap: success

--- src/exercise/11.js
+++ src/exercise/11.js
@@ -6,12 +6,17 @@ import React from 'react'
 
 // 🐨 Make a custom hook called useCounter that accepts the step and
 // initialCount and returns the count and increment functions
-
-function Counter({step = 1, initialCount = 0}) {
-  // 💣 remove this (or move it to your custom hook)
+function useCounter ({step = 1, initialCount = 0}) {
   const [count, setCount] = React.useState(initialCount)
   const increment = () => setCount(c => c + step)
+
+  return [count, increment]
+}
+
+function Counter({step, initialCount}) {
+  // 💣 remove this (or move it to your custom hook)
   // 🐨 Use your custom useCounter hook to get `count` and `increment`
+  const [count, increment] = useCounter({step, initialCount})
   return <button onClick={increment}>{count}</button>
 }
 
--- src/exercise/12.js
+++ src/exercise/12.js
@@ -7,10 +7,13 @@ import React from 'react'
 function Counter({step = 1, initialCount = 0}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 Number(window.localStorage.getItem('count') || initialCount)
-  const [count, setCount] = React.useState(initialCount)
+  const [count, setCount] = React.useState(Number(window.localStorage.getItem('count') || initialCount))
   const increment = () => setCount(c => c + step)
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `count` in localStorage.
+  React.useEffect(() => {
+    window.localStorage.getItem('count', count)
+  })
   return <button onClick={increment}>{count}</button>
 }
 
